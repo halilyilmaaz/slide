024ea2bd85e9d30298f7fb26b5513d5b00c6a880
halilyilmaaz <haliilylmaaz@gmail.com>
2022-02-28 13:37:09 +0300

    indicator width fix

--- lib/slider/SlideWidget/view/slider_widget_view.dart
+++ lib/slider/SlideWidget/view/slider_widget_view.dart
@@ -18,39 +18,42 @@ class SliderWidgetView extends StatelessWidget {
     final controller = Get.put(SliderWidgetController());
     return Scaffold(
       key: controller.scaffoldKey,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          SizedBox(
-            height: MediaQuery.of(context).size.height *
-                widgetOptions!.widgetHeight!,
-            width:
-                MediaQuery.of(context).size.width * widgetOptions!.widgetWidth!,
-            child: PageView.builder(
-                controller: controller.pageController,
-                onPageChanged: (int _index) {
-                  controller.imagePositionIndex = _index;
-                },
-                itemCount: widgetOptions!.widgets.length,
-                pageSnapping: true,
-                itemBuilder: (context, pagePosition) {
-                  return Container(
-                    margin: EdgeInsets.only(top: 5, bottom: 5),
-                    child: widgetOptions!.widgets[pagePosition],
-                  );
-                }),
-          ),
-          Obx(() => indicators(context, 1, controller.imagePositionIndex))
-        ],
+      body: Padding(
+        padding: const EdgeInsets.only(left: 15, right: 15),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            SizedBox(
+              height: MediaQuery.of(context).size.height *
+                  widgetOptions!.widgetHeight!,
+              width:
+                  MediaQuery.of(context).size.width * widgetOptions!.widgetWidth!,
+              child: PageView.builder(
+                  controller: controller.pageController,
+                  onPageChanged: (int _index) {
+                    controller.imagePositionIndex = _index;
+                  },
+                  itemCount: widgetOptions!.widgets.length,
+                  pageSnapping: true,
+                  itemBuilder: (context, pagePosition) {
+                    return Container(
+                      margin: EdgeInsets.only(top: 5, bottom: 5),
+                      child: widgetOptions!.widgets[pagePosition],
+                    );
+                  }),
+            ),
+            Obx(() => indicators(context, 1, controller.imagePositionIndex))
+          ],
+        ),
       ),
     );
   }
 
   Widget indicators(BuildContext context, imagesLength, currentIndex) {
-    double width = 1.0;
+    double width =  0.945;
     return Container(
       height: sliderWidgetOptions!.barHeight,
-      width: MediaQuery.of(context).size.width * width,
+      width: MediaQuery.of(context).size.width,
       decoration: BoxDecoration(
           color: sliderWidgetOptions!.barColor,
           borderRadius: BorderRadius.circular(sliderWidgetOptions!.radius!)),
